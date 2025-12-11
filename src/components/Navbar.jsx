6b17bdabfa8b2636caed1636d1a276a452747f0d
BiSu20042003 <dbikram621@gmail.com>
2025-12-11 13:15:52 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-const Navbar = () => {
+const Navbar = ({ setSidebarOpen }) => {
   const { currUser, logout } = useAuth();
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState("");
